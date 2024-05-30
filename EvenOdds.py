8ea8029692f7932518fcab97a3d58d62d22ccaa4
ParnikaSarbahi <144117040+ParnikaSarbahi@users.noreply.github.com>
2024-05-30 10:36:15 +0530

    Improvec EvenOdds.py

--- EvenOdds.py
+++ EvenOdds.py
@@ -1,3 +1,4 @@
+'''
 s=(input()).split()
 n=int(s[0])
 k=int(s[1])
@@ -11,3 +12,11 @@ else:
         print(1+(k-1)*2)    
     else:
         print((k-(n//2)-1)*2)
+'''
+#better version
+n, k = map(int, input().split()) #niceee
+if k <= n-n//2:
+    print(2*k-1)
+else:
+    k -= n-n//2
+    print(2*k)
